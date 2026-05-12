11395dd926d7ddebf18ff019d944f4c7b4bd1901
elmerleon820-art <elmer.leon820@gmail.com>
2026-05-11 22:52:08 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
-function abrirCarta() {
-  const mensaje = document.getElementById("mensaje");
-  mensaje.style.display = "block";
-  explosionCorazones();
+const paginas = document.querySelectorAll(".pagina");
+let paginaActual = 0;
+
+function cambiarPagina() {
+  paginas[paginaActual].classList.remove("activa");
+
+  paginaActual++;
+
+  if (paginaActual >= paginas.length) {
+    paginaActual = 0;
+  }
+
+  paginas[paginaActual].classList.add("activa");
 }
 
 function crearCorazon() {
@@ -10,49 +19,14 @@ function crearCorazon() {
   corazon.innerHTML = "❤";
 
   corazon.style.left = Math.random() * 100 + "vw";
-  corazon.style.fontSize = 16 + Math.random() * 32 + "px";
-  corazon.style.animationDuration = 5 + Math.random() * 6 + "s";
+  corazon.style.fontSize = 16 + Math.random() * 30 + "px";
+  corazon.style.animationDuration = 5 + Math.random() * 5 + "s";
 
   document.body.appendChild(corazon);
 
   setTimeout(() => {
     corazon.remove();
-  }, 11000);
+  }, 10000);
 }
 
-function crearBrillo() {
-  const brillo = document.createElement("div");
-  brillo.classList.add("brillo");
-
-  brillo.style.left = Math.random() * 100 + "vw";
-  brillo.style.top = Math.random() * 100 + "vh";
-  brillo.style.animationDuration = 1 + Math.random() * 2 + "s";
-
-  document.body.appendChild(brillo);
-
-  setTimeout(() => {
-    brillo.remove();
-  }, 4000);
-}
-
-function explosionCorazones() {
-  for (let i = 0; i < 24; i++) {
-    const heart = document.createElement("div");
-    heart.classList.add("explosion");
-    heart.innerHTML = "❤";
-
-    heart.style.left = "50%";
-    heart.style.top = "50%";
-    heart.style.setProperty("--x", `${Math.random() * 320 - 160}px`);
-    heart.style.setProperty("--y", `${Math.random() * 320 - 160}px`);
-
-    document.body.appendChild(heart);
-
-    setTimeout(() => {
-      heart.remove();
-    }, 1000);
-  }
-}
-
-setInterval(crearCorazon, 350);
-setInterval(crearBrillo, 250);
+setInterval(crearCorazon, 300);
